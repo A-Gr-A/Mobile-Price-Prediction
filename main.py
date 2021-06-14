4244ff12512a6b93512c52656f8b17c3883edadc
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2021-06-14 22:24:30 +0530

    no luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-app = Flask(__name__,static_folder='./client/build',static_url_path='')
+app = Flask(__name__,static_folder='./client/build',static_url_path='/')
 
 @app.route("/")
 def index():
