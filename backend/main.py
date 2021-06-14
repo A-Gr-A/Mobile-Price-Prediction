9acf637b55e8adcc481818a1503781ffcd02ce0e
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2021-06-14 21:14:09 +0530

    trying to deploy

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-  
+app = Flask(__name__,static_foler='./../client/build',static_url_path='/')
+@app.route("/")
+def index():
+   return app.send_static_file('index.html')
+
 @app.route("/api",methods=['GET','POST'])
 def index():
    if request.method =='GET':
@@ -17,4 +20,5 @@ def index():
       return "success"
   
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+   # app.run(debug = True)
