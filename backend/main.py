833bbc4e7eb1c37669635401b70b8b774e168c1b
AnishKacham <f20190091@hyderabad.bits-pilani.ac.in>
2021-06-14 15:51:16 +0530

    configured backend for rest API

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
   
-@app.route("/")
+@app.route("/api")
 def index():
-   return "Need to Link"
+   return {
+      "user":"guest",
+       "id":1,
+       "title":"backedn server"
+   }
   
 if __name__ == '__main__':
    app.run(debug = True)
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 import chart from './logo4.svg';
 // import {Button} from '@material-ui/core'
 import UserForm from './components/UserForm'
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    fetch('/api').then(
+      response=>response.json()
+      )
+      .then(
+        data=>console.log(data)
+      )
+  });
   return (
     <div className="App">
       <img src={chart} style={{maxWidth:"761.98px",width:"100%"}} alt='chart' className='chart-logo'></img>
