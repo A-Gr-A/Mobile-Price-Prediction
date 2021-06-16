d2dd6358990aa5b1e33ff5d10e406af87c94da66
pranay-simejia <simejiapranay@gmail.com>
2021-06-16 14:51:25 +0530

    setup fastAPI to deploy

---
+++ X.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class X(BaseModel):
+    batCap:float
+    blu:float
+    clkSpeed:float
+    dsim:float
+    fc:float	
+    fourg:float	
+    intMem:float	
+    mobDepth:float
+    mobWeight:float 	
+    cpuCores:float
+    pc:float
+    pxh:float
+    pxw:float
+    ram	:float
+    sh:float
+    sw:float
+    tt:float
+    threeg:float
+    tou:float
+    wif:float
--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-from flask import Flask, render_template, request
-app = Flask(__name__,static_folder='./client/build',static_url_path='/')
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import numpy as np
+import pandas as pd
+import joblib
+from X import X
 
-@app.route("/")
-def index():
-   return app.send_static_file('index.html')
+app = FastAPI()
+# app.mount("/", StaticFiles(directory="./client/build", html='index.html'), name="static")
 
-@app.route("/api",methods=['GET','POST'])
-def methodhanlers():
-   if request.method =='GET':
-      print("in get request")
-      return {
-         "user":"guest",
-         "id":1,
-         "title":"backend server"
-      }
-   if request.method == 'POST':
-      print('in post request')
-      print(type(request.json))
-      print(request.json)
-      return "success"
-  
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
-   # app.run(debug = True)
+def preprocess_input(df):
+  df['px_area'] = df['pxw'] * df['pxh'] / (1024**2)
+  df['battery_per_core'] = df['batCap']/ df['cpuCores']
+  df['speed_per_core'] = df['clkSpeed']/ df['cpuCores']
+  df['battery_per_ram'] = df['batCap']/ df['ram']
+  df['speed_per_ram'] = df['clkSpeed']/ df['ram']
+  df['battery_talk'] = df['batCap']/ df['tt']
+  df['memory_per_ram'] = df['intMem']* 1024 / (df['ram'])
+  return df
+ 
+df_cols = ['battery_power','blue','clock_speed','dual_sim','fc','four_g',
+           'int_memory','m_dep','mobile_wt','n_cores','pc','px_height',
+           'px_width','ram','sc_h','sc_w','talk_time','three_g',
+           'touch_screen','wifi']
+ 
+dict_cols = ['batCap', 'blu', 'clkSpeed', 'dsim', 'fc', 'fourg', 
+             'intMem', 'mobDepth', 'mobWeight', 'cpuCores', 'pc',
+             'pxh', 'pxw', 'ram', 'sh', 'sw', 'tt', 'threeg',
+             'tou', 'wif']
+
+
+ 
+ 
+def get_prediction(X_in):
+  d = pd.DataFrame(columns= df_cols)
+ 
+  entries = [float(X_in[col]) for col in dict_cols]
+  entries = pd.Series(entries, index = df_cols)
+  d.loc[len(d)] = entries
+ 
+  d = preprocess_input(d)
+  filename = 'final_model.sav'
+  model = joblib.load(filename)
+  print( int(model.predict(d)))
+
+@app.post('/api')
+def predict(X_in:X):
+   print(X_in)
+   X_in = X_in.dict()
+   X_in = preprocess_input(X_in)
+   get_prediction(X_in)
+   return {
+      "ans": 'get_prediction(X_in)' 
+   }
+
+if __name__ =='__main__':
+      uvicorn.run(app,host='127.0.0.1', port = 8000)
